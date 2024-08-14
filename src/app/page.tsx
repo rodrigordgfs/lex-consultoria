022d6ac6a86f8ea9a18dbdf4b145ace02c80696f
Rodrigo Viegas Rodrigues <rodrigordgfs@gmail.com>
2024-08-13 21:25:01 -0300

    feat(app): Adicionado a hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col">
+      <div className="w-full max-w-full h-screen max-h-screen bg-hero bg-no-repeat bg-cover">
+        teste
+      </div>
+    </main>
+  );
 }
