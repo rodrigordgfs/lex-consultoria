2760c4ae6c60921fb24b7dda06b1dafd0bc0e9e7
Rodrigo Viegas Rodrigues <rodrigordgfs@gmail.com>
2024-08-15 02:11:08 -0300

    fix(app) Ajuste na Hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,11 @@ export default function Home() {
   return (
     <main>
       <div className="w-full max-w-full bg-hero bg-no-repeat bg-cover">
-        <div className="flex flex-col max-w-6xl w-full mx-auto px-4">
-          <Header />
-          <Hero />
+        <div className="bg-hero bg-center bg-cover flex items-center justify-center">
+          <div className="flex flex-col max-w-6xl w-full mx-auto px-4">
+            <Header />
+            <Hero />
+          </div>
         </div>
         <About />
         <Cases />
